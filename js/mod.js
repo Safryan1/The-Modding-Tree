c7a32f31defb149940ea3b642d0b613e52563920
Safryan1 <ryandgrevengoed@icloud.com>
2024-10-26 23:07:43 -0400

    Name
    Added a name to the Mod!!!!

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Sigma Tree",
+	id: "asdfubg234hbg",
+	author: "Safryan",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
@@ -13,8 +13,8 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "1.0",
+	name: "The Sigma Tree",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
